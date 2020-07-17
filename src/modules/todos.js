9fccd5110efb05c25f195c3a44499d407dc317cf
jygg1209 <jygg1209@gmail.com>
2020-07-17 23:42:58 +0900

    Redux practice : immer

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions }  from 'redux-actions';
+import produce from 'immer';
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
@@ -36,23 +37,26 @@ const initialState = {
 
 const todos = handleActions(
   {
-    [CHANGE_INPUT]: (state, {payload:input}) => ({ ...state, input: input}),
-    [INSERT]: (state,{payload:todo}) => ({
-      ...state,
-      todos: state.todos.concat(todo),
+    [CHANGE_INPUT]: (state, { payload:input }) => 
+      produce(state, draft => {
+      draft.input = input;
     }),
-    [TOGGLE]:(state, {payload:id}) => ({
-      ...state,
-      todos: state.todos.map(todo =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo,
-        ),
+    [INSERT]: (state, { payload:todo }) =>
+      produce(state, draft => {
+      draft.todos.push(todo)
     }),
-    [REMOVE]:(state, {payload:id})=>({
-      ...state,
-      todos: state.todos.filter(todo => todo.id !== id),
+    [TOGGLE]:(state, { payload:id }) => 
+      produce(state, draft => {
+        const todo = draft.todos.find(todo => todo.id ===id);
+        todo.done = !todo.done;
     }),
+    [REMOVE]:(state, { payload:id }) =>
+      produce(state, draft => {
+        const index = draft.todos.indeIndex(todo => todo.id ===id );
+        draft.todos.splice(index, 1);
+      }),
+    },
     initialState,
-  }
-)
+);
 
 export default todos;
